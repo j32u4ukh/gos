038f69d5fcecfe8548feb2c784bea848c94a5283
j32u4ukh <j32u4ukh@gmail.com>
2023-01-15 21:38:55 +0800

    style: 調整變數名稱

--- ans/http2.go
+++ ans/http2.go
@@ -28,11 +28,11 @@ type HttpAnser2 struct {
 
 	// ==================================================
 	// Request & Response
-	// 個數與 Anser 的 nConnect 相同，因此可利用 Conn 中的 id 作為索引值，來存取 rrStates, Request 與 Response
+	// 個數與 Anser 的 nConnect 相同，因此可利用 Conn 中的 id 作為索引值，來存取 , Request 與 Response
 	// 由於是使用 Conn 的 id 作為索引值，因此可以不用從第一個開始使用，結束使用後也不需要對順序進行調整
 	// ==================================================
-	contexts    []*ghttp.Context
-	currContext *ghttp.Context
+	httpConns []*ghttp.Context
+	httpConn  *ghttp.Context
 
 	// Temp variables
 	lineString string
@@ -45,8 +45,8 @@ func NewHttpAnser2(laddr *net.TCPAddr, nConnect int32, nWork int32) (IAnswer, er
 			ghttp.MethodGet:  {},
 			ghttp.MethodPost: {},
 		},
-		contexts:    make([]*ghttp.Context, nConnect),
-		currContext: nil,
+		httpConns: make([]*ghttp.Context, nConnect),
+		httpConn:  nil,
 	}
 
 	// ===== Anser =====
@@ -67,7 +67,7 @@ func NewHttpAnser2(laddr *net.TCPAddr, nConnect int32, nWork int32) (IAnswer, er
 	// ===== R2 =====
 	var i int32
 	for i = 0; i < nConnect; i++ {
-		a.contexts[i] = ghttp.NewContext(i)
+		a.httpConns[i] = ghttp.NewContext(i)
 	}
 
 	//////////////////////////////////////////////////
@@ -88,62 +88,62 @@ func (a *HttpAnser2) Listen() {
 
 func (a *HttpAnser2) read() bool {
 	// 根據 Conn 的 Id，存取對應的 R2
-	a.currContext = a.contexts[a.currConn.GetId()]
+	a.httpConn = a.httpConns[a.currConn.GetId()]
 
 	// 讀取 第一行
-	if a.currContext.State == 0 {
-		if a.currConn.CheckReadable(a.currContext.HasLineData) {
-			a.currConn.Read(&a.readBuffer, a.currContext.ReadLength)
+	if a.httpConn.State == 0 {
+		if a.currConn.CheckReadable(a.httpConn.HasLineData) {
+			a.currConn.Read(&a.readBuffer, a.httpConn.ReadLength)
 
 			// 拆分第一行數據
-			a.lineString = strings.TrimRight(string(a.readBuffer[:a.currContext.ReadLength]), "\r\n")
+			a.lineString = strings.TrimRight(string(a.readBuffer[:a.httpConn.ReadLength]), "\r\n")
 			fmt.Printf("(a *HttpAnser) Read | firstLine: %s\n", a.lineString)
 
-			if a.currContext.Request2.ParseFirstLine(a.lineString) {
-				if a.currContext.Request2.Method == ghttp.MethodGet {
+			if a.httpConn.Request2.ParseFirstLine(a.lineString) {
+				if a.httpConn.Request2.Method == ghttp.MethodGet {
 					// 解析第一行數據中的請求路徑
-					a.currContext.Request2.ParseQuery()
+					a.httpConn.Request2.ParseQuery()
 				}
-				a.currContext.State = 1
+				a.httpConn.State = 1
 				fmt.Printf("(a *HttpAnser) Read | State: 0 -> 1\n")
 			}
 		}
 	}
 
 	// 讀取 Header 數據
-	if a.currContext.State == 1 {
+	if a.httpConn.State == 1 {
 		var key, value string
 		var ok bool
 
-		for a.currConn.CheckReadable(a.currContext.HasLineData) && a.currContext.State == 1 {
+		for a.currConn.CheckReadable(a.httpConn.HasLineData) && a.httpConn.State == 1 {
 			// 讀取一行數據
-			a.currConn.Read(&a.readBuffer, a.currContext.ReadLength)
+			a.currConn.Read(&a.readBuffer, a.httpConn.ReadLength)
 
 			// mustHaveFieldNameColon ensures that, per RFC 7230, the field-name is on a single line,
 			// so the first line must contain a colon.
 			// 將讀到的數據從冒號拆分成 key, value
 			// k, v, ok := bytes.Cut(a.readBuffer[:a.currContext.ReadLength], COLON)
-			a.lineString = strings.TrimRight(string(a.readBuffer[:a.currContext.ReadLength]), "\r\n")
+			a.lineString = strings.TrimRight(string(a.readBuffer[:a.httpConn.ReadLength]), "\r\n")
 			key, value, ok = strings.Cut(a.lineString, ghttp.COLON)
 
 			if ok {
 				// 持續讀取 Header
 				// key := string(k)
 
-				if _, ok := a.currContext.Header[key]; !ok {
-					a.currContext.Header[key] = []string{}
+				if _, ok := a.httpConn.Header[key]; !ok {
+					a.httpConn.Header[key] = []string{}
 				}
 
 				value = strings.TrimLeft(value, " \t")
 				// value = strings.TrimRight(value, "\r\n")
-				a.currContext.Header[key] = append(a.currContext.Header[key], value)
+				a.httpConn.Header[key] = append(a.httpConn.Header[key], value)
 				fmt.Printf("(a *HttpAnser) Read | Header, key: %s, value: %s\n", key, value)
 
 			} else {
 				// 當前這行數據不包含":"，結束 Header 的讀取
 
 				// Header 中包含 Content-Length，狀態值設為 2，等待讀取後續數據
-				if contentLength, ok := a.currContext.Header["Content-Length"]; ok {
+				if contentLength, ok := a.httpConn.Header["Content-Length"]; ok {
 					length, err := strconv.Atoi(contentLength[0])
 					fmt.Printf("(a *HttpAnser) Read | Content-Length: %d\n", length)
 
@@ -152,8 +152,8 @@ func (a *HttpAnser2) read() bool {
 						return false
 					}
 
-					a.currContext.ReadLength = int32(length)
-					a.currContext.State = 2
+					a.httpConn.ReadLength = int32(length)
+					a.httpConn.State = 2
 					fmt.Printf("(a *HttpAnser) Read | State: 1 -> 2\n")
 
 				} else {
@@ -167,7 +167,7 @@ func (a *HttpAnser2) read() bool {
 					a.currWork = a.currWork.Next
 
 					// 等待數據寫出
-					a.currContext.State = 3
+					a.httpConn.State = 3
 					return true
 				}
 			}
@@ -175,27 +175,27 @@ func (a *HttpAnser2) read() bool {
 	}
 
 	// 讀取 Body 數據
-	if a.currContext.State == 2 {
-		if a.currConn.CheckReadable(a.currContext.HasEnoughData) {
+	if a.httpConn.State == 2 {
+		if a.currConn.CheckReadable(a.httpConn.HasEnoughData) {
 			// ==========
 			// 讀取 data
 			// ==========
 			// 將傳入的數據，加入工作緩存中
-			a.currConn.Read(&a.readBuffer, a.currContext.ReadLength)
-			fmt.Printf("(a *HttpAnser) Read | %s\n", string(a.readBuffer[:a.currContext.ReadLength]))
+			a.currConn.Read(&a.readBuffer, a.httpConn.ReadLength)
+			fmt.Printf("(a *HttpAnser) Read | %s\n", string(a.readBuffer[:a.httpConn.ReadLength]))
 
 			// 考慮分包問題，收到完整一包數據傳完才傳到應用層
 			a.currWork.Index = a.currConn.GetId()
 			a.currWork.RequestTime = time.Now().UTC()
 			a.currWork.State = 1
-			a.currWork.Body.AddRawData(a.readBuffer[:a.currContext.ReadLength])
+			a.currWork.Body.AddRawData(a.readBuffer[:a.httpConn.ReadLength])
 			a.currWork.Body.ResetIndex()
 
 			// 指向下一個工作結構
 			a.currWork = a.currWork.Next
 
 			// 等待數據寫出
-			a.currContext.State = 3
+			a.httpConn.State = 3
 
 			return false
 		}
@@ -215,34 +215,34 @@ func (a *HttpAnser2) write(cid int32, data *[]byte, length int32) error {
 	a.currConn.SetWriteBuffer(data, length)
 
 	// 等待數據寫出
-	a.currContext.State = 4
+	a.httpConn.State = 4
 	return nil
 }
 
 // 由外部定義 workHandler，定義如何處理工作
 func (a *HttpAnser2) SetWorkHandler() {
 	a.workHandler = func(w *base.Work) {
-		a.currContext = a.contexts[w.Index]
+		a.httpConn = a.httpConns[w.Index]
 
-		if handler, ok := a.Handlers[a.currContext.Method]; ok {
-			if functions, ok := handler[a.currContext.Query]; ok {
+		if handler, ok := a.Handlers[a.httpConn.Method]; ok {
+			if functions, ok := handler[a.httpConn.Query]; ok {
 				for _, f := range functions {
 
-					f(a.currContext)
+					f(a.httpConn)
 
-					a.currContext.SetHeader("Connection", "close")
+					a.httpConn.SetHeader("Connection", "close")
 
 					// 將 Response 回傳數據轉換成 Work 傳遞的格式
-					bs := a.currContext.ToResponseData()
+					bs := a.httpConn.ToResponseData()
 					fmt.Printf("Response: %s\n", string(bs))
 					w.Body.AddRawData(bs)
 					w.Send()
 				}
 			} else {
-				a.errorRequestHandler(w, a.currContext, "Unregistered http query.")
+				a.errorRequestHandler(w, a.httpConn, "Unregistered http query.")
 			}
 		} else {
-			a.errorRequestHandler(w, a.currContext, "Unregistered http method.")
+			a.errorRequestHandler(w, a.httpConn, "Unregistered http method.")
 		}
 	}
 }
@@ -267,10 +267,10 @@ func (a *HttpAnser2) shouldClose(err error) bool {
 	if a.Anser.shouldClose(err) {
 		return true
 	}
-	a.currContext = a.contexts[a.currConn.GetId()]
-	if a.currContext.State == 4 && a.currConn.WritableLength == 0 {
+	a.httpConn = a.httpConns[a.currConn.GetId()]
+	if a.httpConn.State == 4 && a.currConn.WritableLength == 0 {
 		fmt.Printf("(a *HttpAnser) shouldClose | Conn(%d) 完成數據寫出，準備關閉連線\n", a.currConn.GetId())
-		a.currContext.State = 0
+		a.httpConn.State = 0
 		return true
 	}
 	return false
